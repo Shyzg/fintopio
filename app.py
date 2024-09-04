1abb5a41e06e21a8cfefcfdcacdc00957a5563c0
Al Jarreau Arman Maulana <shyzago@gmail.com>
2024-09-04 16:34:24 +0700

    Aw

--- app.py
+++ app.py
@@ -38,47 +38,43 @@ class Fintopio:
             flush=True
         )
 
-    def referrals_data(self, token: str):
-        url = 'https://fintopio-tg.fintopio.com/api/referrals/data'
+    def activate_referrals(self, token: str):
+        url = 'https://fintopio-tg.fintopio.com/api/referrals/activate'
+        data = json.dumps({'code':'l5bYPIC8FtjMColV'})
         self.headers.update({'Authorization': f'Bearer {token}'})
         try:
-            response = self.session.get(url=url, headers=self.headers)
+            response = self.session.post(url=url, headers=self.headers, data=data)
             response.raise_for_status()
-            referrals_data = response.json()
-            if referrals_data is not None:
-                return referrals_data
-            else:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In Referrals Data ]{Style.RESET_ALL}")
-        except requests.HTTPError as e:
-            self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Fetching State Farming: {str(e)} ]{Style.RESET_ALL}")
-        except requests.RequestException as e:
-            self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ A Request Error Occurred While Fetching State Farming: {str(e)} ]{Style.RESET_ALL}")
-        except Exception as e:
-            self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Fetching State Farming: {str(e)} ]{Style.RESET_ALL}")
+            return True
+        except (Exception, requests.HTTPError, requests.RequestException):
+            return False
 
     def daily_checkins(self, token: str):
         url = 'https://fintopio-tg.fintopio.com/api/daily-checkins'
         self.headers.update({'Authorization': f'Bearer {token}'})
         try:
-            response = self.session.get(url=url, headers=self.headers)
+            response = self.session.post(url=url, headers=self.headers)
             response.raise_for_status()
             daily_checkins = response.json()
             if daily_checkins is not None:
-                self.print_timestamp(
-                    f"{Fore.GREEN + Style.BRIGHT}[ Daily Checkins Claimed ]{Style.RESET_ALL}"
-                    f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-                    f"{Fore.YELLOW + Style.BRIGHT}[ Day {daily_checkins['totalDays']} ]{Style.RESET_ALL}"
-                    f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-                    f"{Fore.BLUE + Style.BRIGHT}[ Reward {daily_checkins['dailyReward']} ]{Style.RESET_ALL}"
-                )
+                if daily_checkins['claimed']:
+                    self.print_timestamp(f"{Fore.MAGENTA + Style.BRIGHT}[ Daily Checkins Already Claimed ]{Style.RESET_ALL}")
+                else:
+                    self.print_timestamp(
+                        f"{Fore.GREEN + Style.BRIGHT}[ Daily Checkins Claimed ]{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+                        f"{Fore.BLUE + Style.BRIGHT}[ Reward {daily_checkins['dailyReward']} ]{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+                        f"{Fore.YELLOW + Style.BRIGHT}[ Day {daily_checkins['totalDays']} ]{Style.RESET_ALL}"
+                    )
             else:
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In Daily Checkins ]{Style.RESET_ALL}")
         except requests.HTTPError as e:
-            self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Fetching State Farming: {str(e)} ]{Style.RESET_ALL}")
+            self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Daily Checkins: {str(e)} ]{Style.RESET_ALL}")
         except requests.RequestException as e:
-            self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ A Request Error Occurred While Fetching State Farming: {str(e)} ]{Style.RESET_ALL}")
+            self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ A Request Error Occurred While Daily Checkins: {str(e)} ]{Style.RESET_ALL}")
         except Exception as e:
-            self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Fetching State Farming: {str(e)} ]{Style.RESET_ALL}")
+            self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Daily Checkins: {str(e)} ]{Style.RESET_ALL}")
 
     def state_farming(self, token: str):
         url = 'https://fintopio-tg.fintopio.com/api/farming/state'
@@ -90,7 +86,7 @@ class Fintopio:
             if state_farming is not None:
                 return state_farming
             else:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In Farm Farming ]{Style.RESET_ALL}")
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In State Farming ]{Style.RESET_ALL}")
         except requests.HTTPError as e:
             self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Fetching State Farming: {str(e)} ]{Style.RESET_ALL}")
         except requests.RequestException as e:
@@ -122,7 +118,8 @@ class Fintopio:
             else:
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In Farm Farming ]{Style.RESET_ALL}")
         except requests.HTTPError as e:
-            if e.response.status_code == 400:
+            error_farm_farming = e.response.json()
+            if e.response.status_code == 400 and error_farm_farming['message'] == 'Farming has been already started':
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ Farming Has Been Already Started ]{Style.RESET_ALL}")
             else:
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Farm Farming: {str(e)} ]{Style.RESET_ALL}")
@@ -152,11 +149,12 @@ class Fintopio:
                         formatted_finish = finish.strftime('%x %X %Z')
                         self.print_timestamp(f"{Fore.YELLOW + Style.BRIGHT}[ Farming Can Be Claim At {formatted_finish} ]{Style.RESET_ALL}")
                 else:
-                    self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No 'state' In Farm Farming ]{Style.RESET_ALL}")
+                    self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No 'state' In Claim Farming ]{Style.RESET_ALL}")
             else:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In Farm Farming ]{Style.RESET_ALL}")
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In Claim Farming ]{Style.RESET_ALL}")
         except requests.HTTPError as e:
-            if e.response.status_code == 400:
+            error_claim_farming = e.response.json()
+            if e.response.status_code == 400 and error_claim_farming['message'] == 'Farming is not finished yet':
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ Farming Is Not Finished Yet ]{Style.RESET_ALL}")
             else:
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Claim Farming: {str(e)} ]{Style.RESET_ALL}")
@@ -175,7 +173,7 @@ class Fintopio:
             if state_diamond is not None:
                 return state_diamond
             else:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In Farm Farming ]{Style.RESET_ALL}")
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In State Diamond ]{Style.RESET_ALL}")
         except requests.HTTPError as e:
             self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Fetching State Diamond: {str(e)} ]{Style.RESET_ALL}")
         except requests.RequestException as e:
@@ -195,11 +193,15 @@ class Fintopio:
             response.raise_for_status()
             self.print_timestamp(f"{Fore.GREEN + Style.BRIGHT}[ Claimed {total_reward} From State Diamond ]{Style.RESET_ALL}")
         except requests.HTTPError as e:
-            error_message_start = e.response.json()
-            if e.response.status_code == 400 and error_message_start['message'] == 'Game is not available at the moment':
+            error_complete_diamond = e.response.json()
+            if e.response.status_code == 400 and error_complete_diamond['message'] == 'Game is not available at the moment':
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ Game Is Not Available At The Moment ]{Style.RESET_ALL}")
-            elif e.response.status_code == 400 and error_message_start['message'] == 'The diamond is outdated, reload the page and try again':
+            elif e.response.status_code == 400 and error_complete_diamond['message'] == 'The diamond is outdated, reload the page and try again':
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ The Diamond Is Outdated, Reload The Page And Try Again ]{Style.RESET_ALL}")
+            elif e.response.status_code == 400 and error_complete_diamond['message'] == 'Game is already finished, please wait until the next one is available':
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ The Diamond Is Outdated, Reload The Page And Try Again ]{Style.RESET_ALL}")
+            elif e.response.status_code == 400 and error_complete_diamond['message']['diamondNumber']['isNumberString'] == 'diamondNumber must be a number string':
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ Diamond Number Must Be A Number String ]{Style.RESET_ALL}")
             else:
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Complete Diamond: {str(e)} ]{Style.RESET_ALL}")
         except requests.RequestException as e:
@@ -240,14 +242,12 @@ class Fintopio:
                     sleep(random.choice([10, 20]))
                     self.claim_tasks(token=token, task_id=task_id, task_slug=task_slug, task_reward_amount=task_reward_amount)
                 elif start_tasks['status'] == 'in-progress':
-                    self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ Please Try This Task By Itself ]{Style.RESET_ALL}")
-                else:
-                    self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ I Can Not Do Anything When 'status' In Start Tasks Is {start_tasks['status']} ]{Style.RESET_ALL}")
+                    self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ Finish This {task_slug} By Itself ]{Style.RESET_ALL}")
             else:
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In Start Tasks ]{Style.RESET_ALL}")
         except requests.HTTPError as e:
-            error_message_start = e.response.json()
-            if e.response.status_code == 400 and error_message_start['message'] == 'Unable to update task status':
+            error_start_tasks = e.response.json()
+            if e.response.status_code == 400 and error_start_tasks['message'] == 'Unable to update task status':
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ Unable To Update Task Status. Please Try This Task By Itself ]{Style.RESET_ALL}")
             else:
                 self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Start Tasks: {str(e)} ]{Style.RESET_ALL}")
@@ -271,19 +271,16 @@ class Fintopio:
                             f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
                             f"{Fore.BLUE + Style.BRIGHT}[ Reward {task_reward_amount} ]{Style.RESET_ALL}"
                         )
-                        break
-                    else:
-                        self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ I Can Not Do Anything When 'status' In Claim Tasks Is {claim_tasks['status']} ]{Style.RESET_ALL}")
-                        break
+                    break
                 else:
                     self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ There Is No Data In Start Tasks ]{Style.RESET_ALL}")
                     break
             except requests.HTTPError as e:
-                error_message_claim = e.response.json()
-                if e.response.status_code == 400 and error_message_claim['message'] == 'Entity not found':
+                error_claim_tasks = e.response.json()
+                if e.response.status_code == 400 and error_claim_tasks['message'] == 'Entity not found':
                     self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {task_slug} Not Found ]{Style.RESET_ALL}")
                     break
-                elif e.response.status_code == 400 and error_message_claim['message'] == 'Unable to update task status':
+                elif e.response.status_code == 400 and error_claim_tasks['message'] == 'Unable to update task status':
                     self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ Please Wait Until {task_slug} Is Claimed ]{Style.RESET_ALL}")
                     sleep(random.choice([5, 10]))
                 else:
@@ -306,16 +303,9 @@ class Fintopio:
                 all_next_at_times = []
                 
                 for index, token in enumerate(tokens):
-                    referrals_data = self.referrals_data(token=token)
-                    if referrals_data['isDailyRewardClaimed']:
-                        self.print_timestamp(
-                            f"{Fore.CYAN + Style.BRIGHT}[ {index + 1} ]{Style.RESET_ALL}"
-                            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-                            f"{Fore.BLUE + Style.BRIGHT}[ Balance {referrals_data['balance']} ]{Style.RESET_ALL}"
-                        )
-                    else:
-                        self.print_timestamp(f"{Fore.CYAN + Style.BRIGHT}[ {index + 1} ]{Style.RESET_ALL}")
-                        self.daily_checkins(token=token)
+                    self.print_timestamp(f"{Fore.CYAN + Style.BRIGHT}[ {index + 1} ]{Style.RESET_ALL}")
+                    self.activate_referrals(token=token)
+                    self.daily_checkins(token=token)
                 self.print_timestamp(f"{Fore.WHITE + Style.BRIGHT}[ Farming ]{Style.RESET_ALL}")
                 for index, token in enumerate(tokens):
                     self.print_timestamp(f"{Fore.CYAN + Style.BRIGHT}[ {index + 1} ]{Style.RESET_ALL}")
@@ -346,7 +336,7 @@ class Fintopio:
                     state_diamond = self.state_diamond(token=token)
                     if state_diamond['state'] == 'available':
                         self.complete_diamond(token=token, diamond_number=state_diamond['diamondNumber'], total_reward=state_diamond['settings']['totalReward'])
-                    elif state_diamond['state'] == 'unavailable':
+                    elif state_diamond['state'] in ['unavailable', 'failed', 'completed']:
                         next_at = datetime.fromtimestamp(state_diamond['timings']['nextAt'] / 1000).astimezone()
                         all_next_at_times.append(next_at)
                         formatted_next_at = next_at.strftime('%x %X %Z')
